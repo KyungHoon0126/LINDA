31a727b836d424f9a62e9ce49ff9096dc6117e64
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-10-07 13:20:54 +0900

    Add files via upload

---
+++ schoolInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINDA
+{
+    public class schoolInfo
+    {
+        public string date
+        {
+            get;
+            set;
+        }
+        public string id
+        {
+            get;
+            set;
+        }
+        public string content
+        {
+            get;
+            set;
+        }
+    }
+}
